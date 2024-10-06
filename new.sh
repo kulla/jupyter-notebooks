65dd84f00f34f7b1bfba386086605c19d25a7360
Stephan Kulla <git.mail@kulla.me>
2024-10-06 16:56:06 +0200

    chore: Create new notebook from template

--- new.sh
+++ new.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 NOTEBOOK_DIR="notebooks"
+NOTEBOOK_TITLE="$@"
 
-# Get the current year, month, and day from the system's date
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
 DAY=$(date +%d)
 
-# Prompt the user to enter a filename, read it into the FILENAME variable, and
-# format it
-read -p "Enter filename: " INPUT
-
 # Convert the input string to lowercase, replace spaces with hyphens, and trim
 # leading/trailing spaces or hyphens.
-FILENAME=$(echo "$INPUT" | tr '[:upper:]' '[:lower:]' | tr -s ' ' '-' | sed 's/^[ \-]*//;s/[ \-]*$//')
+FILENAME=$(echo "$TITLE" | tr '[:upper:]' '[:lower:]' | tr -s ' ' '-' | sed 's/^[ \-]*//;s/[ \-]*$//')
 
 # Create the directory structure if it doesn't exist
 DIR="$NOTEBOOK_DIR/$YEAR"
@@ -21,6 +17,12 @@ mkdir -p "$DIR"
 
 # Define the notebook path
 NOTEBOOK_PATH="$DIR/$YEAR-$MONTH-$DAY-$FILENAME.ipynb"
+
+# Create the new jupyter notebook
+cp template.ipynb "$NOTEBOOK_PATH"
+
+sed -i "s/NOTEBOOK_TITLE/$NOTEBOOK_TITLE/g" "$NOTEBOOK_PATH"
+sed -i "s/YEAR/$YEAR/g" "$NOTEBOOK_PATH"
 
 # Open the jupyter notebook
 jupyter lab --notebook-dir="$NOTEBOOK_DIR" "$NOTEBOOK_PATH"
