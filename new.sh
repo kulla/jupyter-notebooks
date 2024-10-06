b3f5f1f31ad983c6d9b1f15bdc21532408a3b863
Stephan Kulla <git.mail@kulla.me>
2024-10-06 17:01:02 +0200

    fix(new.sh): Use correct variable name

--- new.sh
+++ new.sh
@@ -9,7 +9,7 @@ DAY=$(date +%d)
 
 # Convert the input string to lowercase, replace spaces with hyphens, and trim
 # leading/trailing spaces or hyphens.
-FILENAME=$(echo "$TITLE" | tr '[:upper:]' '[:lower:]' | tr -s ' ' '-' | sed 's/^[ \-]*//;s/[ \-]*$//')
+FILENAME=$(echo "$NOTEBOOK_TITLE" | tr '[:upper:]' '[:lower:]' | tr -s ' ' '-' | sed 's/^[ \-]*//;s/[ \-]*$//')
 
 # Define the notebook path
 NOTEBOOK_PATH="$NOTEBOOK_DIR/$YEAR/$YEAR-$MONTH-$DAY-$FILENAME.ipynb"
